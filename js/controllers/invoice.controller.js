ab6b77d10e4a378b2361156c3d822be36868d311
Guilherme Sanches <guilherme_1993pa@yahoo.com.br>
2016-11-24 18:54:08 -0200

    Correção de case com string

--- js/controllers/invoice.controller.js
+++ js/controllers/invoice.controller.js
@@ -53,56 +53,56 @@ App.controller('InvoiceCtrl', ['$scope', 'md5', 'HomeService', function ($scope,
            if(!baixa_renda){
                 $scope.tot = tot*bandeiras[bandeira-1];
            }else{               
-                   switch (bandeira) {
-                       case "1" && $scope.tot <= 30.00:
+                   switch (true) {
+                       case (bandeira =="1" && $scope.tot <= 30.00):
                           $scope.tot = tot*0.18247;
                            break;
-                       case "1" &&  $scope.tot >= 31 &&  $scope.tot <= 100.99:
+                       case (bandeira =="1" &&  $scope.tot >= 31 &&  $scope.tot <= 100.99):
                            $scope.tot = tot*0.31280;
                            break;
-                       case  "1" &&  $scope.tot >= 101 &&  $scope.tot <= 220.99:
+                       case (bandeira == "1" &&  $scope.tot >= 101 &&  $scope.tot <= 220.99):
                            $scope.tot = tot*0.46921;
                            break;
-                       case "1" &&  $scope.tot >= 221:
+                       case (bandeira =="1" &&  $scope.tot >= 221):
                            $scope.tot = tot*0.52134;
                            break;
 
-                      case "2" && $scope.tot <= 30.00:
+                      case (bandeira =="2" && $scope.tot <= 30.00):
                           $scope.tot = tot*0.18772;
                            break;
-                       case "2" &&  $scope.tot >= 31 &&  $scope.tot <= 100.99:
+                       case (bandeira =="2" &&  $scope.tot >= 31 &&  $scope.tot <= 100.99):
                            $scope.tot = tot*0.32180;
                            break;
-                       case "2" &&  $scope.tot >= 101 &&  $scope.tot <= 220.99:
+                       case (bandeira =="2" &&  $scope.tot >= 101 &&  $scope.tot <= 220.99):
                            $scope.tot = tot*0.48271;
                            break;
-                       case "2" &&  $scope.tot >= 221:
+                       case (bandeira =="2" &&  $scope.tot >= 221):
                            $scope.tot = tot*0.53634;
                            break;
 
-                      case "3" && $scope.tot <= 30.00:
+                      case (bandeira =="3" && $scope.tot <= 30.00):
                           $scope.tot = tot*0.19297;
                            break;
-                       case "3" &&  $scope.tot >= 31 &&  $scope.tot <= 100.99:
+                       case (bandeira =="3" &&  $scope.tot >= 31 &&  $scope.tot <= 100.99):
                            $scope.tot = tot*0.33080;
                            break;
-                       case "3" &&  $scope.tot >= 101 &&  $scope.tot <= 220.99:
+                       case (bandeira =="3" &&  $scope.tot >= 101 &&  $scope.tot <= 220.99):
                            $scope.tot = tot*0.49621;
                            break;
-                       case "3" &&  $scope.tot >= 221:
+                       case (bandeira =="3" &&  $scope.tot >= 221):
                            $scope.tot = tot*0.55134;
                            break;
 
-                      case "4" && $scope.tot <= 30.00:
+                      case (bandeira =="4" && $scope.tot <= 30.00):
                           $scope.tot = tot*0.19822;
                            break;
-                       case "4" &&  $scope.tot >= 31 &&  $scope.tot <= 100.99:
+                       case (bandeira =="4" &&  $scope.tot >= 31 &&  $scope.tot <= 100.99):
                            $scope.tot = tot*0.33980;
                            break;
-                       case "4" &&  $scope.tot >= 101 &&  $scope.tot <= 220.99:
+                       case (bandeira =="4" &&  $scope.tot >= 101 &&  $scope.tot <= 220.99):
                            $scope.tot = tot*0.50971;
                            break;
-                       case "4" &&  $scope.tot >= 221:
+                       case (bandeira =="4" &&  $scope.tot >= 221):
                            $scope.tot = tot*0.56634;
                            break;                           
                    }
